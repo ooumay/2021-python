ec3f4ab1cef8d1eb4900589aee7f88f8e799bf27
ooumay <Kiry321.rthtll@gmail.com>
2021-10-28 14:32:46 +0300

    Update lesson.py

--- ИС-31Б/Плёнкин/lesson.py
+++ ИС-31Б/Плёнкин/lesson.py
@@ -12,10 +12,16 @@ import math
 
 dict_1 = {'P': 16,'L': 12,'E': 5,'N': 14,'K': 11,'I': 9,'N': 14}
 
-values = list(dict_1.values())
+values = list(dict_1.values()) # Список значений
+print(values)
 
-winter_values = [i ** 3 for i in values]
+spring_values = ()
 
+for i in values:
+    x = math.factorial(i)
+    
+
+winter_values = [i ** 3 for i in values] # в) Возводим значения в 3-ю степень
 print(winter_values)
     
 
